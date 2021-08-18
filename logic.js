a34101d151459cecf0e7d4e1d8631fcd0b93a7f4
Muhammad Raiyaan <83336994+muhammedraiyaan2@users.noreply.github.com>
2021-08-18 13:48:03 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -2,10 +2,10 @@ let btn=document.getElementById('btn')
 btn.addEventListener("click",function(){
     let user=document.getElementById('userId')
     let password=document.getElementById('password')
-    localStorage.setItem("userId",user)
-    localStorage.setItem("password",password)
+    localStorage.setItem("userIds",user.value)
+    localStorage.setItem("passwords",password.value)
     document.write(`<h1 style="color:green; text-align:center;">
 Successfully saved your user id and password</h1>`)
-    document.write(`<h1>your user name is</h1>`,user.value+"<br>");
-    document.write(`<h1>your password is</h1>`,password.value)
+    document.write(`<h1>your user name is</h1>`,localStorage.getItem("userIds")+"<br>");
+    document.write(`<h1>your password is</h1>`,localStorage.getItem("passwords"))
 })
